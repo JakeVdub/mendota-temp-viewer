0c8de27bba6c67169e7d130945fca82127bb5d0c
Jake VandeWalle <jakevandewalle@gmail.com>
2021-10-04 20:19:25 -0500

    Merge WIP branch into main (#1)
    * Clean up UI
    
    * Reorganize before things get too complicated
    
    * Add Chart.js library
    
    * Add chart.js library, get basic line graph showing up
    
    * Actually pull in real data from UW's API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import TempChart from './components/TempChart';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const API_URL = "http://metobs.ssec.wisc.edu/api/data.json?site=mendota&inst=buoy&symbols=water_temp_1:water_temp_10&begin=-24:00:00&interval=1h";
+
+  useEffect(() => {
+    fetch(API_URL).then((response) => response.json()).then((data) => {
+      let apiResults = data.results;
+      let dataArr = [];
+
+      for (let i = 0; i < apiResults.data.length; i++) {
+        dataArr.push({
+          'timestamp': apiResults.timestamps[i],
+          '0ftTempData': apiResults.data[i][0]
+        });
+      }
+
+      setWaterTempData(dataArr);
+    });
+  }, []); // Using an empty array for the dependencies means this will only be called one time (onLoad).
+
+  const [waterTempData, setWaterTempData] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="Body">
+        <TempChart waterTempData={waterTempData} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../styles/Header.css';
+
+export default function Header() {
+    return (
+        <header className="App-header">
+            <p>
+            Welcome to the Mendota Water Temp Viewer!
+            </p>
+      </header>
+    );
+};
