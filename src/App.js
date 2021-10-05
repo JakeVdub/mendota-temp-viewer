e6d6fa720e9de06ec63eb1412e8a852097444d3f
JakeVdub <jakevandewalle@gmail.com>
2021-10-04 20:15:03 -0500

    Actually pull in real data from UW's API

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import TempChart from './components/TempChart';
 import { useState, useEffect } from 'react';
 
 function App() {
+  const API_URL = "http://metobs.ssec.wisc.edu/api/data.json?site=mendota&inst=buoy&symbols=water_temp_1:water_temp_10&begin=-24:00:00&interval=1h";
 
   useEffect(() => {
-    // TODO THIS IS WHERE WE RETRIEVE AND STORE THE TEMP DATA
-    console.log("Retrieve data...");
-    setTempData([60, 61, 62, 61, 59, 60, 65, 60, 58, 57, 55, 56]);
+    fetch(API_URL).then((response) => response.json()).then((data) => {
+      let apiResults = data.results;
+      let dataArr = [];
+
+      for (let i = 0; i < apiResults.data.length; i++) {
+        dataArr.push({
+          'timestamp': apiResults.timestamps[i],
+          '0ftTempData': apiResults.data[i][0]
+        });
+      }
+
+      setWaterTempData(dataArr);
+    });
   }, []); // Using an empty array for the dependencies means this will only be called one time (onLoad).
 
-  const [tempData, setTempData] = useState([]);
+  const [waterTempData, setWaterTempData] = useState([]);
 
   return (
     <div className="App">
       <Header />
       <div className="Body">
-        <TempChart tempData={tempData} />
+        <TempChart waterTempData={waterTempData} />
       </div>
     </div>
   );
--- src/components/TempChart.js
+++ src/components/TempChart.js
@@ -2,20 +2,42 @@ import '../styles/TempChart.css';
 import {Line} from 'react-chartjs-2';
 import { useState, useEffect } from 'react';
 
-export default function TempChart({tempData}) {
+export default function TempChart({waterTempData}) {
+    console.log(waterTempData);
     const [chartData, setChartData] = useState({});
+
+    // Helper function for formating a Date object as a string ("MM/DD HH:MM")
+    const convertDateToString = (dateObj) => {
+        let dateStr = (dateObj.getMonth() + 1) + "/" + dateObj.getDate();
+        let hours = dateObj.getHours().toString().length > 1 ? dateObj.getHours() : "0" + dateObj.getHours();
+        let minutes = dateObj.getMinutes().toString().length > 1 ? dateObj.getMinutes() : "0" + dateObj.getMinutes();
+        return dateStr + " " + hours + ":" + minutes;
+    };
 
     const updateChartData = () => {
         setChartData({
-            labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            labels: waterTempData.map((dataStruct) => {
+                let date = new Date(dataStruct['timestamp']);
+                return convertDateToString(date);
+            }),
             datasets: [
                 {
-                    label: "temperature",
-                    data: tempData,
+                    label: "0ft",
+                    data: waterTempData.map((dataStruct) => dataStruct['0ftTempData']),
                     backgroundColor: [ 'rgba(255, 255, 255, 1)' ],
-                    borderWidth: 4
-                }
-            ]
+                    borderColor: "#FFF",
+                    borderWidth: 4,
+                    cubicInterpolationMode: "monotone"
+                },
+                // {
+                //     label: "3ft",
+                //     data: [60, 60, 60, 62, 59, 60, 60, 60, 59, 58, 59, 60],
+                //     backgroundColor: [ 'rgba(255, 255, 255, 1)' ],
+                //     borderColor: "#555",
+                //     borderWidth: 4,
+                //     cubicInterpolationMode: "monotone"
+                // }
+            ],
         });
     }
 
