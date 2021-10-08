6a0f693d691a6fa439576838b6f1a2da3580f742
JakeVdub <jakevandewalle@gmail.com>
2021-10-07 22:14:17 -0500

    Major layout enhancements
      * Add axis lable
      * Add title
      * Add tooltips to graph (hover text)

--- src/components/TempChart.js
+++ src/components/TempChart.js
@@ -2,9 +2,33 @@ import '../styles/TempChart.css';
 import { Line } from 'react-chartjs-2';
 
 export default function TempChart({ waterTempData }) {
+    const options = {
+        plugins: {
+            legend: {
+                position: "bottom",
+                title: {
+                    display: true,
+                    text: "Water Depth"
+                }
+            },
+            tooltip: {
+                mode: 'index',
+                intersect: false
+            },
+        },
+        scales: {
+            y: {
+                title: {
+                    display: true,
+                    text: "Temp (F)"
+                }    
+            }
+        }
+    };
     return (
         <div className="Chart">
-            <Line data={waterTempData} />
+            <h1>Water Temps Over Last 24hrs</h1>
+            <Line data={waterTempData} options={options} />
         </div>
     );
 };
